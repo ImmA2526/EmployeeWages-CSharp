cb3e7aca61e0734284e725128077c99f698d9ca1
Avenger2627 <imraninfo97@gmail.com>
2020-12-02 13:48:53 +0530

    [Imran Shaikh] Add: Employee Wage Calculation for 20 Days Added

--- Program.cs
+++ Program.cs
@@ -7,27 +7,33 @@ namespace EmployeeWage
         const int PartTime = 1;
         const int FullTime = 2;
         const int WagePerHour = 20;
+        public static int WorkDay = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome To Employee Wages Problem***************");
-            int Salary = 0;
-            int Wage = 0;
-            Random Number = new Random();
-            int Num = Number.Next(0, 3);
-            switch (Num)
+            int Salary = 0, Wage = 0, TotalWage = 0;
+
+            for (WorkDay = 0; WorkDay <= 20; WorkDay++)
             {
-                case FullTime:
-                    Wage = 8;
-                    break;
-                case PartTime:
-                    Wage = 4;
-                    break;
-                default:
-                    Wage = 0;
-                    break;
+                Random Number = new Random();
+                int Num = Number.Next(0, 3);
+                switch (Num)
+                {
+                    case FullTime:
+                        Wage = 8;
+                        break;
+                    case PartTime:
+                        Wage = 4;
+                        break;
+                    default:
+                        Wage = 0;
+                        break;
+                }
+                Salary = Wage * WagePerHour;
+                TotalWage += Salary;
+                Console.WriteLine("Salary: " + Salary);
             }
-            Salary = Wage * WagePerHour;
-            Console.WriteLine("Employee is Present and Salary is: " + Salary);
+            Console.WriteLine("Employee is Present and Salary is: " + TotalWage);
         }
     }
 }
