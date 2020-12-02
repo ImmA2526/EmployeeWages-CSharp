a263cc36b60605fbfc59b284e7c6096b9c334685
Avenger2627 <imraninfo97@gmail.com>
2020-12-02 14:07:51 +0530

    [Imran Shaikh] Add: Employee Wage Calculation for Company Added

--- Program.cs
+++ Program.cs
@@ -6,14 +6,21 @@ namespace EmployeeWage
     {
         const int PartTime = 1;
         const int FullTime = 2;
-        const int WagePerHour = 20;
-        public static int WorkDay = 20;
-        public static int WorkHrs = 100;
-        public static void ComputeWage()
+        private int WagePerHour;
+        private int WorkDay;
+        private int WorkHrs;
+        private string Company;
+        public Program(string Company, int WagePerHour, int WorkDay, int WorkHrs)
+        {
+            this.Company = Company;
+            this.WorkDay = WorkDay;
+            this.WagePerHour = WagePerHour;
+            this.WorkHrs = WorkHrs;
+        }
+        public void ComputeWage()
         {
 
-            int Salary = 0, Wage = 0, TotalWage = 0;
-            int TotalHrs = 0, TotalDay = 0;
+            int Salary = 0, Wage = 0,TotalHrs = 0, TotalDay = 0;
 
             while (TotalHrs <= WorkHrs && TotalDay < WorkDay)
             {
@@ -36,14 +43,18 @@ namespace EmployeeWage
                 TotalHrs += Wage;
                 Console.WriteLine("Day:  " + TotalDay + "  Emp Hrs: " + Wage);
             }
-            Salary = TotalHrs * WagePerHour;
-            Console.WriteLine("Employee is Present and Salary is: " + Salary);
+            Salary = TotalHrs * this.WagePerHour;
+            Console.WriteLine("Total Employee Wage for Company:  "+Company+ "is:" +Salary);
         }
-
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome To Employee Wages Problem***************");
-            ComputeWage();
+            Program Dmart = new Program("Telco", 10, 3, 12);
+            Program Wipro = new Program("Wipro", 10, 2, 12);
+            Program Telegram = new Program("Telegram", 10, 2, 12);
+            Dmart.ComputeWage();
+            Wipro.ComputeWage();
+            Telegram.ComputeWage();
         }
     }
 }
