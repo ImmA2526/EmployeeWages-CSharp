622605eff4a8f35e6f7a350dafea509270379374
Avenger2627 <imraninfo97@gmail.com>
2020-12-02 13:36:02 +0530

    [Imran Shaikh] Add: Check Employee is Present or Not Added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
 namespace EmployeeWage
 {
     class Program
     {
+        const int FullTime = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome To Employee Wages Problem***************");
+            Random Number = new Random();
+            int Num = Number.Next(0, 2);
+            if (Num == FullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Not Present");
+            }
         }
     }
 }
