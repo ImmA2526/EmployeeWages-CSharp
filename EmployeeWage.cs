6a9e3da0b579a6133ab9b4e59302eb8e33812cf8
Avenger2627 <imraninfo97@gmail.com>
2020-12-03 11:24:20 +0530

    [Imran Shaikh] Add:Employee Wages For MultipleCompanies Added

---
+++ EmployeeWage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class EmployeeWage
+    {
+        public const int PartTime = 1;
+        public const int FullTime = 2;
+        public int NumofCompany = 0;
+        public CompanyEmpWage[] CompanyWageArray;
+       
+        public EmployeeWage()
+        {
+            this.CompanyWageArray = new CompanyEmpWage[5];
+        }
+
+        public void AddCompanyWage(string Company, int WagePerHour, int WorkDay, int WorkHrs)
+        {
+            CompanyWageArray[NumofCompany] = new CompanyEmpWage(Company, WagePerHour, WorkDay, WorkHrs);
+            NumofCompany++;
+        }
+
+        public void ComputeWages()
+        {
+            for (int i = 0; i < NumofCompany; i++)
+            {
+                CompanyWageArray[i].TotalWage(this.ComputeWage(this.CompanyWageArray[i]));
+                Console.WriteLine(this.CompanyWageArray[i].toString());
+            }
+        }
+
+        private int ComputeWage(CompanyEmpWage CompanyEmpWage)
+        {
+            int Wage = 0, TotalHrs = 0, TotalDay = 0;
+            while (TotalHrs <= CompanyEmpWage.WorkHrs && TotalDay < CompanyEmpWage.WorkDay)
+            {
+                TotalDay++;
+                Random Number = new Random();
+                int Num = Number.Next(0, 3);
+                switch (Num)
+                {
+                    case FullTime:
+                        Wage = 8;
+                        break;
+                    case PartTime:
+                        Wage = 4;
+                        break;
+                    default:
+                        Wage = 0;
+                        break;
+                }
+                TotalHrs += Wage;
+                Console.WriteLine("Day:  " + TotalDay + "  Emp Hrs: " + Wage);
+            }
+            return TotalHrs * CompanyEmpWage.WagePerHour;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,60 +1,16 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace EmployeeWages
 {
-    class Program
-    {
-        const int PartTime = 1;
-        const int FullTime = 2;
-        private int WagePerHour;
-        private int WorkDay;
-        private int WorkHrs;
-        private string Company;
-        public Program(string Company, int WagePerHour, int WorkDay, int WorkHrs)
-        {
-            this.Company = Company;
-            this.WorkDay = WorkDay;
-            this.WagePerHour = WagePerHour;
-            this.WorkHrs = WorkHrs;
-        }
-        public void ComputeWage()
-        {
-
-            int Salary = 0, Wage = 0,TotalHrs = 0, TotalDay = 0;
-
-            while (TotalHrs <= WorkHrs && TotalDay < WorkDay)
-            {
-                TotalDay++;
-                Random Number = new Random();
-                int Num = Number.Next(0, 3);
-                switch (Num)
-                {
-                    case FullTime:
-                        Wage = 8;
-                        break;
-                    case PartTime:
-                        Wage = 4;
-                        break;
-                    default:
-                        Wage = 0;
-                        break;
-                }
-                //Salary = Wage * WagePerHour;
-                TotalHrs += Wage;
-                Console.WriteLine("Day:  " + TotalDay + "  Emp Hrs: " + Wage);
-            }
-            Salary = TotalHrs * this.WagePerHour;
-            Console.WriteLine("Total Employee Wage for Company:  "+Company+ "is:" +Salary);
-        }
+    public class Program
+    {   
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome To Employee Wages Problem***************");
-            Program Dmart = new Program("Telco", 10, 3, 12);
-            Program Wipro = new Program("Wipro", 10, 2, 12);
-            Program Telegram = new Program("Telegram", 10, 2, 12);
-            Dmart.ComputeWage();
-            Wipro.ComputeWage();
-            Telegram.ComputeWage();
+            EmployeeWage Result = new EmployeeWage();
+            Result.AddCompanyWage("Telegram", 12, 3, 10);
+            Result.AddCompanyWage("Jio", 14, 4, 18);
+            Result.ComputeWages();
         }
     }
 }
