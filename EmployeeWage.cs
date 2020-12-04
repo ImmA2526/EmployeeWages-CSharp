17cd07982f9e5f59441bc5cada4d69e7af1741c3
Imran2526 <68658753+Imran2526@users.noreply.github.com>
2020-12-04 13:13:38 +0530

    [Imran Shaikh ] Refactor:  Employee Wage

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -1,40 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EmployeeWages
 {
-    class EmployeeWage: IComputeEmpWage
+    class EmployeeWage : IComputeEmpWage
     {
         public const int PartTime = 1;
         public const int FullTime = 2;
-        public int NumofCompany = 0;
-        public CompanyEmpWage[] CompanyWageArray;
+        private LinkedList <CompanyEmpWage> CompanyEmpWageList;
         private Dictionary<string, CompanyEmpWage> CompanytoEmpWageMap;
 
         public EmployeeWage()
         {
             this.CompanytoEmpWageMap = new Dictionary<string, CompanyEmpWage>();
-            this.CompanyWageArray = new CompanyEmpWage[5];
+            this.CompanyEmpWageList = new LinkedList<CompanyEmpWage>();
         }
 
         public void AddCompanyWage(string Company, int WagePerHour, int WorkDay, int WorkHrs)
         {
-            CompanyWageArray[NumofCompany] = new CompanyEmpWage(Company, WagePerHour, WorkDay, WorkHrs);
-            CompanyEmpWage CompanyEmpWage =new CompanyEmpWage(Company, WagePerHour, WorkDay, WorkHrs);
-            CompanytoEmpWageMap.Add(Company, CompanyEmpWage);
-            NumofCompany++;
+            CompanyEmpWage CompanyEmpWage = new CompanyEmpWage(Company, WagePerHour, WorkDay, WorkHrs);
+            this.CompanytoEmpWageMap.Add(Company, CompanyEmpWage);
+            this.CompanyEmpWageList.AddLast(CompanyEmpWage);
         }
-
-        public void ComputeWages()
+      public void ComputeWages()
         {
-            for (int i = 0; i < NumofCompany; i++)
+            foreach (CompanyEmpWage companyEmpWage in this.CompanyEmpWageList)
             {
-                CompanyWageArray[i].TotalWage(this.ComputeWage(this.CompanyWageArray[i]));
-                Console.WriteLine(this.CompanyWageArray[i].toString());
+                companyEmpWage.TotalWage(this.ComputeWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.toString());
             }
         }
-
+      
         private int ComputeWage(CompanyEmpWage CompanyEmpWage)
         {
             int Wage = 0, TotalHrs = 0, TotalDay = 0;
